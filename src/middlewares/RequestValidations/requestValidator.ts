c0b40b59500ce1cf385d92269208611f5b4b50c7
RUYANGA <ruyangam15@gmail.com>
2025-05-01 18:40:33 +0200

    Addnew

--- src/middlewares/RequestValidations/requestValidator.ts
+++ src/middlewares/RequestValidations/requestValidator.ts
@@ -1,6 +1,7 @@
 import {body} from 'express-validator'
+import { PrismaClient } from '@prisma/client'
 
-
+const prisma=new PrismaClient()
 
 export const RegisterValidation=[
     body('name')
@@ -14,6 +15,17 @@ export const RegisterValidation=[
     .normalizeEmail()
     .withMessage('Email required !')
     .escape()
+    .custom((value,{req})=>{
+        return prisma.user.findUnique({
+            where:{email:value}
+        }).then(user=>{
+            if(user){
+                return Promise.reject(
+                    'User with email already exist'
+                )
+            }
+        })
+    })
     .isLength({min:5})
     .withMessage('Password must be at least 5 digits'),
     body('reg_no')
--- src/routes/UserRoutes/index.ts
+++ src/routes/UserRoutes/index.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import {Register} from '../../controllers/user'
+import {RegisterValidation} from '../../middlewares/RequestValidations/requestValidator'
+import { validateRequest } from "../../middlewares/Auth/errorValiations";
 
 
 const router=Router()
-router.post('/user/register',Register)
+router.post('/user/register',RegisterValidation,validateRequest,Register)
 
 
 export default router
