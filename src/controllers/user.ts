b1f10cad5405bba47171e163c44932182f06b2f6
RUYANGA <ruyangam15@gmail.com>
2025-05-01 18:47:40 +0200

    Addnew

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -52,4 +52,25 @@ export async function Register(req:Request,res:Response,next:NextFunction):Promi
         console.log(error)
         return res.status(500).json({Error:'Error to register user '})
     }
+};
+
+
+export async function resendOtp(req:Request,res:Response,next:NextFunction){
+    interface resendInput{
+        email:string
+    }
+    const {email}=req.body
+
+    const user =await prisma.user.findUnique({
+        where:{email:email}
+    })
+
+    if(!user)return res.status(404).json({Error:'User not found'})
+
+    const otp:string= await crypto.randomInt(111111,999999).toString();
+    const expiredOtp:Date=addMinutes(new Date(),15);
+
+    sendEmail(otp,email,user?.name)
+    
+
 }
