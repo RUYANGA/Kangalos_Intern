5b9d1b015ac3dc7afba393229577f4c23030bd9a
RUYANGA <ruyangam15@gmail.com>
2025-05-01 18:25:08 +0200

    Addnew

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -11,41 +11,45 @@ const prisma=new PrismaClient()
 
 export async function Register(req:Request,res:Response,next:NextFunction):Promise<any>{
 
-    interface userInput{
-        email:string,
-        name:string,
-        password:string,
-        gender:string,
-        reg_no:number
-    }
-
-    const{name,email,reg_no,gender,password}:userInput=req.body;
-
-    const otp:string= await crypto.randomInt(111111,999999).toString();
-    const expiredOtp=addMinutes(new Date(),15)
-    const hashPassword=await bcrypt.hash(password,12)
-
-
-
-    const user=await prisma.user.create({
-        data:{
-            name,
-            email,
-            reg_no,
-            password:hashPassword,
-            gender,
+    try {
+        interface userInput{
+            email:string,
+            name:string,
+            password:string,
+            gender:string,
+            reg_no:number
         }
-    })
-
-    await prisma.otp.create({
-       data:{
-        otp:otp,
-        expiredDate:expiredOtp,
-        userId:user.id
-       }
-    })
-
-    sendEmail(email,otp,user.id,)
-
-    res.status(201).json({Message:'User registered'})
+    
+        const{name,email,reg_no,gender,password}:userInput=req.body;
+    
+        const otp:string= await crypto.randomInt(111111,999999).toString();
+        const expiredOtp=addMinutes(new Date(),15)
+    
+        const hashPassword=await bcrypt.hash(password,12)     
+    
+        const user=await prisma.user.create({
+            data:{
+                name,
+                email,
+                reg_no,
+                password:hashPassword,
+                gender,
+            }
+        })
+    
+        await prisma.otp.create({
+           data:{
+            otp:otp,
+            expiredDate:expiredOtp,
+            userId:user.id
+           }
+        })
+    
+        sendEmail(email,otp,user.name,)
+    
+        res.status(201).json({Message:'User registered'})
+    } catch (error) {
+        console.log(error)
+        return res.status(500).json({Error:'Error to register user '})
+    }
 }
