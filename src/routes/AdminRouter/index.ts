75c5ff358a8fe170939b1242147645447c0c02f9
RUYANGA <ruyangam15@gmail.com>
2025-05-06 16:01:30 +0200

    Addnew

--- src/routes/AdminRouter/index.ts
+++ src/routes/AdminRouter/index.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import {AddUniversity,updateUniversity,AddCollege,updateCollege} from '../../controllers/Users/AdminController/user'
+import {AddUniversity,updateUniversity,AddCollege,updateCollege,deleteCollege} from '../../controllers/Users/AdminController/user'
 import {AuthorizeRoles, AuthorizTeam} from '../../middlewares/Auth/TokenVerify'
 
 const router=Router();
@@ -8,5 +8,6 @@ router.post('/admin/university',AuthorizeRoles(['ADMIN']),AddUniversity);
 router.put('/admin/updateUniversity/:id',AuthorizeRoles(['ADMIN']),updateUniversity);
 router.post('/admin/university/:id/dean/:deanId',AuthorizeRoles(['ADMIN']),AddCollege);
 router.put('/admin/college/:id/update',AuthorizeRoles(['ADMIN']),updateCollege);
+router.delete('/admin/delete/college/:id',deleteCollege)
 
 export default router
