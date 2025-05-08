e3b35d2dd4e428e48e5e42758be988ab78b2684d
RUYANGA <ruyangam15@gmail.com>
2025-05-08 15:01:27 +0200

    Addnew

---
+++ prisma/migrations/20250508125729_change_unique_reg_no/migration.sql
@@ -0,0 +1,2 @@
+-- DropIndex
+DROP INDEX "User_reg_no_key";
--- src/routes/AdminRouter/index.ts
+++ src/routes/AdminRouter/index.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import {AddUniversity,updateUniversity,AddCollege,updateCollege,deleteCollege,AddCollegeDirector,addSchool} from '../../controllers/Users/AdminController/user'
+import {AddUniversity,updateUniversity,AddCollege,updateCollege,deleteCollege,AddCollegeDirector,addSchool,AddDean} from '../../controllers/Users/AdminController/user'
 import {AuthorizeRoles, AuthorizTeam} from '../../middlewares/Auth/TokenVerify'
 import { AddCollegeValidation } from "../../middlewares/RequestValidations/requestValidator";
 import {validateRequest} from '../../middlewares/Auth/errorValiations'
@@ -14,6 +14,6 @@ router.post('/admin/college/:id/adddirector',AuthorizeRoles(['ADMIN']),AddColleg
 
 router.put('/admin/college/:id/update',AuthorizeRoles(['ADMIN']),updateCollege);  //Update colleges
 router.delete('/admin/delete/college/:id',AuthorizeRoles(['ADMIN']),deleteCollege)  //Delete college with id
-router.post('/admin/college/:id/addschool',AuthorizeRoles(['ADMIN']),addSchool)
-
+router.post('/admin/college/:id/addschool',AuthorizeRoles(['ADMIN']),addSchool);
+router.post('/admin/school/:id/adddean',AuthorizeRoles(['ADMIN']),AddDean);
 export default router
