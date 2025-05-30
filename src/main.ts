96de4c18c7c72334e7ee0a3d445bbea18e087e10
RUYANGA <ruyangam15@gmail.com>
2025-05-30 14:58:55 +0200

    Add cors

--- src/main.ts
+++ src/main.ts
@@ -16,8 +16,11 @@ const port=process.env.PORT||2000
 
 const app=express()
 
-app.use(express.json())
+app.use(express.json());
+
+
 app.use(cors({
+    origin:'*',
     methods:['GET','POST','PUT','PUCH'],
     credentials:true
 }))
