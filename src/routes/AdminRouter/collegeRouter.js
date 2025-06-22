8eb834965dd7182809de28abe979e693a678fac4
rukundowilson <rukundowilson5@gmail.com>
2025-06-22 17:30:25 +0200

    removed build files

--- src/routes/AdminRouter/collegeRouter.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var express_1 = require("express");
-var colleges_1 = require("../../controllers/Admin/colleges");
-var college_1 = require("../../middlewares/zod/college");
-var zodValidaate_1 = require("../../middlewares/Auth/zodValidaate");
-var router = (0, express_1.Router)();
-router.post('/admin/addcollege/:id', (0, zodValidaate_1.zodValidate)(college_1.AddCollegeSchema), colleges_1.AddCollege);
-exports.default = router;
--- src/routes/AdminRouter/schoolRouter.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var express_1 = require("express");
-var schools_1 = require("../../controllers/Admin/schools");
-var school_1 = require("../../middlewares/zod/school");
-var zodValidaate_1 = require("../../middlewares/Auth/zodValidaate");
-var router = (0, express_1.Router)();
-router.post('/admin/addschool/:id', (0, zodValidaate_1.zodValidate)(school_1.AddSchoolSchema), schools_1.addSchool);
-exports.default = router;
