9b20423b2a2aac9a1fbaec5603691e5e582d9e79
RUYANGA <ruyangam15@gmail.com>
2025-06-23 21:41:01 +0200

    update university

--- src/controllers/Admin/Universities.ts
+++ src/controllers/Admin/Universities.ts
@@ -5,6 +5,7 @@ import { error } from "console";
 
 
 
+
 export async function AddUniversity(req:Request<{},{},AddUniversityDto>,res:Response,next:NextFunction):Promise<any>{
 
    
@@ -62,6 +63,10 @@ export async function getUniversity(req:Request,res:Response,next:NextFunction):
             }
         })
 
+         if (university.length === 0) {
+            return res.status(404).json({ message: "No university found." });
+        }
+
         res.status(200).json({
             message:'University get successfully!',
             university:university
@@ -71,6 +76,77 @@ export async function getUniversity(req:Request,res:Response,next:NextFunction):
         console.error(error);
         return res.status(500).json({
         error: 'Error get university',
+        });
+    }
+
+}
+
+export async function updateUniversity(req:Request<{id:string},{},AddUniversityDto>,res:Response,next:NextFunction):Promise<any>{
+
+    try {
+        const universityId=req.params.id;
+
+        const result=await AddUniversitySchema.safeParseAsync(req.body);
+
+        if(!result.success){
+            return res.status(400).json({
+                errors:result.error.format()
+            })
+        }
+        const data:AddUniversityDto=result.data
+
+        const updated=await prisma.university.update({
+            where:{
+                id:universityId
+            },
+            data:{
+                name:data.name,
+                description:data.description,
+                location:data.location
+
+            }
+        });
+
+        res.status(201).json({
+            message:'University updated',
+            university:updated
+        })
+        
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({
+        error: 'Error update university',
+        });
+    }
+
+
+}
+
+export async function deleteUniversity(req:Request,res:Response,next:NextFunction):Promise<any>{
+    try {
+
+        const universityId=req.params.id
+
+        const deleted=await prisma.university.delete({
+            where:{
+                id:universityId
+            }
+        });
+
+        if(!deleted){
+            return res.status(200).json({
+                message:'University deleted successfully'
+            })
+        }
+
+        res.status(200).json({
+            message:'University deleted successfully'
+        })
+        
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({
+        error: 'Error delete university',
         });
     }
 
